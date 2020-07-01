5fca0280bb2a89f9caea2b0a0553f23b5ec845a9
Yx_Zou <hello.yxhome@gmail.com>
2020-07-01 19:00:01 +0800

    测试延迟渲染

--- reactweb/src/App.js
+++ reactweb/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import ReactDOM from 'react-dom';
 function App() {
     function formatName(user) {
         return user.firstName + ' ' + user.lastName;
@@ -10,9 +10,12 @@ function App() {
         lastName: '456',
     };
     const element = <h1>Hello, {formatName(name)}</h1>;
-    return (
-        element
-    );
+    setTimeout(() => {
+        return ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+    }, 4000)
+    return element
+
+
 }
 
 export default App;
