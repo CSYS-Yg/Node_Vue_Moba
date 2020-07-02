ca25f9d6282eb57a1d0158f96b7d4c4414fb7c64
Yx_Zou <hello.yxhome@gmail.com>
2020-07-03 00:04:48 +0800

    完成 web 部分页面修改

---
+++ web/src/pages/fitness/addNotes/components/squat.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <a-divider orientation="left">深蹲目标</a-divider>
+        <p>
+            第一组：训练重量：80% * 次数：6 次
+            <br />第二组：训练重量：80% * 次数：6 次
+            <br />第三组：训练重量：80% * 次数：6 次
+            <br />第四组：训练重量：80% * 次数：6 次
+            <br />
+        </p>
+        <a-form-model-item label="第一组">
+            重量：
+            <a-input-number v-model="form.fieldA" :min="0" />KG * 次数：
+            <a-input-number v-model="form.fieldA" :min="0" />次
+        </a-form-model-item>
+        <a-form-model-item label="第二组">
+            重量：
+            <a-input-number v-model="form.fieldA" :min="0" />KG * 次数：
+            <a-input-number v-model="form.fieldA" :min="0" />次
+        </a-form-model-item>
+        <a-form-model-item label="第四组">
+            重量：
+            <a-input-number v-model="form.fieldA" :min="0" />KG * 次数：
+            <a-input-number v-model="form.fieldA" :min="0" />次
+        </a-form-model-item>
+        <a-form-model-item label="第三组">
+            重量：
+            <a-input-number v-model="form.fieldA" :min="0" />KG * 次数：
+            <a-input-number v-model="form.fieldA" :min="0" />次
+        </a-form-model-item>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                fieldA: "",
+                fieldB: "",
+                fieldC: "",
+                fieldD: ""
+            }
+        };
+    }
+};
+</script>
+
+<style lang="stylus" scoped></style>
--- web/src/pages/fitness/addNotes/index.vue
+++ web/src/pages/fitness/addNotes/index.vue
@@ -1,19 +1,56 @@
 <template>
-    <div>123</div>
+    <div class="page-box">
+        <a-date-picker
+            :default-value="moment(new Date(), dateFormat)"
+            :allowClear="false"
+            inputReadOnly
+            @change="onChange"
+        />
+        <span>{{week}}</span>
+
+        <a-form-model
+            layout="horizontal"
+            :label-col="{ span: 1 }"
+            :wrapper-col="{ span: 6 }"
+        >
+            <squat></squat>
+            <a-form-model-item>
+                <a-button type="primary">Submit</a-button>
+            </a-form-model-item>
+        </a-form-model>
+    </div>
 </template>
 
 <script>
 import date from "~/assets/util/reuseJs/date.js";
+import squat from "./components/squat";
+import moment from "moment";
 export default {
+    componets: {
+        squat
+    },
     data() {
+        this.dateFormat = "YYYY-MM-DD";
         return {
             week: ""
         };
     },
     mounted() {
-        this.week = date.getWeekDate();
+        this.week = date.getWeekDate(new Date());
+    },
+    methods: {
+        moment,
+        onChange(dateJson) {
+            this.week = date.getWeekDate(dateJson._d);
+        }
     }
 };
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.page-box
+    margin 15px
+    padding 15px
+    background rgba(255, 255, 255, 1)
+    border-radius 8px
+</style>
