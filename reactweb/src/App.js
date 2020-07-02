c043e1a7b717cabe235b25678f0eb1bb7f11100e
Yx_Zou <hello.yxhome@gmail.com>
2020-07-02 11:49:40 +0800

    react 组件测试

--- reactweb/src/App.js
+++ reactweb/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
 function App() {
     function formatName(user) {
         return user.firstName + ' ' + user.lastName;
@@ -9,9 +14,9 @@ function App() {
         firstName: '123',
         lastName: '456',
     };
-    const element = <h1>Hello, {formatName(name)}</h1>;
+    const element = (<div> <Welcome name="Sara" /> <Welcome name="Sara1" /> <Welcome name="Sara2" /> </div>)
     setTimeout(() => {
-        return ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+        return ReactDOM.render(<h1>Hello, {formatName(name)}</h1>, document.getElementById('root'));
     }, 4000)
     return element
 
