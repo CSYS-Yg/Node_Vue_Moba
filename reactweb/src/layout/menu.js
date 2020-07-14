b73c99075d4bd7700f04f3ea0c250adbd52506f0
Yx_Zou <hello.yxhome@gmail.com>
2020-07-14 11:34:28 +0800

    添加菜单栏注释

--- reactweb/src/layout/menu.js
+++ reactweb/src/layout/menu.js
@@ -1,12 +1,23 @@
 
 /* 
-{
-    menuItem: '',  // 单主菜单
-    manyMenuItem:[{
-    
-    }]
-    menuItem: '',  // 单主菜单
-} 
+{       // 单菜单
+        title: '首页',  //  第一层级名字
+        id: 'mail',    //  选中的 key
+        iconClass: 'iconsousuo', //图标
+        subMenu: [{  
+            typeName: '训练计划1',  // 第二层级分类
+            childItem: [{          // 分类下的子菜单
+                title: '锻炼',
+                id: '',
+                iconClass: 'iconsousuo',
+            },
+            {
+                title: '锻炼2',
+                id: '',
+                iconClass: 'iconsousuo',
+            }]
+        },
+},
 */
 const menu = [
     {
