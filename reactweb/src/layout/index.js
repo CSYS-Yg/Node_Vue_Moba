8ef67bbf0061cc94456082d06422a42d43b14eec
Yx_Zou <hello.yxhome@gmail.com>
2020-07-14 11:32:14 +0800

    完成菜单列表动态渲染

--- reactweb/src/layout/index.js
+++ reactweb/src/layout/index.js
@@ -5,10 +5,18 @@ import Icon from '../assets/iconfont/index'
 
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import menuList from './menu'
+
 
 const { Header, Content, Footer } = Layout;
 const { SubMenu } = Menu;
+
+const menuListDom = menuList.map((menu) =>
+    <Menu.Item key={menu.id} icon={<Icon type={menu.icon} />}>{menu.title}</Menu.Item>
+);
+
 class ReactLayout extends React.Component {
+
     state = {
         current: 'mail',
     };
@@ -27,24 +35,26 @@ class ReactLayout extends React.Component {
                 <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
                     <div className="logo" />
                     <Menu onClick={this.handleClick} selectedKeys={[current]} theme="dark" mode="horizontal">
-                        <Menu.Item key="mail" icon={<Icon type='iconsousuo' />}>
-                            Navigation One</Menu.Item>
-                        <Menu.Item key="app" disabled icon={<Icon type='iconsousuo' />}>
-                            Navigation Two</Menu.Item>
-                        <SubMenu icon={<Icon type='iconsousuo' />} title="Navigation Three - Submenu">
-                            <Menu.ItemGroup title="Item 1">
-                                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                                <Menu.Item key="setting:2">Option 2</Menu.Item>
-                            </Menu.ItemGroup>
-                            <Menu.ItemGroup title="Item 2">
-                                <Menu.Item key="setting:3">Option 3</Menu.Item>
-                                <Menu.Item key="setting:4">Option 4</Menu.Item>
-                            </Menu.ItemGroup>
-                        </SubMenu>
-                        <Menu.Item key="alipay">
-                            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                                Navigation Four - Link</a>
-                        </Menu.Item>
+                        {
+                            menuList.map((menu) => {
+                                if (menu.subMenu && menu.subMenu.length > 0) {
+                                    return <SubMenu icon={<Icon type={menu.iconClass} />} title={menu.title}>
+                                        {menu.subMenu.map((subMenu) => {
+                                            return <Menu.ItemGroup title={subMenu.typeName}>
+                                                {
+                                                    subMenu.childItem.map((child) => {
+                                                        return <Menu.Item key={child.id}>{child.title}</Menu.Item>
+                                                    })
+                                                }
+                                            </Menu.ItemGroup>
+                                        })}
+                                    </SubMenu>
+                                } else {
+                                    return <Menu.Item key={menu.id} icon={<Icon type={menu.iconClass} />}>{menu.title}</Menu.Item>
+                                }
+                            }
+                            )
+                        }
                     </Menu>
                 </Header>
                 <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
