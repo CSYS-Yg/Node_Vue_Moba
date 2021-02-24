d57be22ac6139e1b0ecce6a23849eb9868c5ddd6
YuGuangWork <linyusir@gmail.com>
2021-02-24 16:01:05 +0800

    新增本地数据配置文件

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
+const Deploy = require('./config')
+console.log("🚀 ~ file: index.js ~ line 2 ~ Deploy", Deploy.deploy)
+
 // 引入 express
 
 const express = require('express');
+
 
 // 创建一个 express 类
 const app = express();
@@ -10,12 +14,7 @@ const mysql = require('mysql');
 const e = require('express');
 
 //实现本地链接
-const db = mysql.createConnection({
-    host: 'localhost', // 地址
-    user: 'root',      // 数据库名 
-    password: 'yuguang', // 密码
-    database: 'nodemysql' //数据库名字，未创建不需配置
-})
+const db = mysql.createConnection(Deploy.deploy)
 
 db.connect((err) => {
     if (err) throw err;
@@ -31,106 +30,106 @@ app.all("*", function (req, res, next) {
     next();
 })
 
-// 创建数据库测试
-app.get('/create', (req, res) => {
-    let sql = "CREATE DATABASE nodemysql"
-    db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(result);
-            res.send('DateBase create success ...')
-        }
-    })
-})
+// // 创建数据库测试
+// app.get('/create', (req, res) => {
+//     let sql = "CREATE DATABASE nodemysql"
+//     db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(result);
+//             res.send('DateBase create success ...')
+//         }
+//     })
+// })
 
-// 创建数据表
-app.get('/create_table', (req, res) => {
-    let sql = "CREATE TABLE posts(id int AUTO_INCREMENT,title VARCHAR(255),body VARCHAR(255),PRIMARY KEY(ID))"
-    db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(result);
-            res.send('创建表格成功')
-        }
-    })
-})
+// // 创建数据表
+// app.get('/create_table', (req, res) => {
+//     let sql = "CREATE TABLE posts(id int AUTO_INCREMENT,title VARCHAR(255),body VARCHAR(255),PRIMARY KEY(ID))"
+//     db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(result);
+//             res.send('创建表格成功')
+//         }
+//     })
+// })
 
-// 递归 拼接菜单
-let meunList = []
-function spliceMeun(parentId, childen) {
-    for (let i = 0; i < meunList.length; i++) {
-        if (meunList[i].parents_id == parentId) {
-            meunList[i].childItem = []
-            childen.push(meunList[i])
-            spliceMeun(meunList[i].id, meunList[i].childItem)
-        }
-    }
-    return childen
-}
+// // 递归 拼接菜单
+// let meunList = []
+// function spliceMeun(parentId, childen) {
+//     for (let i = 0; i < meunList.length; i++) {
+//         if (meunList[i].parents_id == parentId) {
+//             meunList[i].childItem = []
+//             childen.push(meunList[i])
+//             spliceMeun(meunList[i].id, meunList[i].childItem)
+//         }
+//     }
+//     return childen
+// }
 
-// 查询数据表
-app.get('/getMeunList', (req, res) => {
-    let sql = `SELECT * FROM meun`
-    db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
-        if (err) {
-            console.log(err)
-        } else {
-            meunList = result
-            res.send({ data: spliceMeun(null, []) })
-        }
-    })
-})
+// // 查询数据表
+// app.get('/getMeunList', (req, res) => {
+//     let sql = `SELECT * FROM meun`
+//     db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             meunList = result
+//             res.send({ data: spliceMeun(null, []) })
+//         }
+//     })
+// })
 
 
-function setGroup(list) {
-    let title = ['第 0 组', '第一组', '第二组', '第三组', '第四组', '第五组', '第六组', '第七组', '第八组',]
-    list.forEach((element, index) => {
-        list[index]['title'] = title[element.group_times]
-    });
-    return list
-}
-// 查询当前计划组次
-app.get('/getPlanGroup', (req, res) => {
-    let sql = `
-    SELECT distinct my_plan.group_times
-    FROM my_plan
-    WHERE my_plan.training_date = '${req.query.training_date}'
-    `
-    db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
-        if (err) {
-            console.log(err)
-        } else {
-            res.send({ data: setGroup(result) })
-        }
-    })
-})
+// function setGroup(list) {
+//     let title = ['第 0 组', '第一组', '第二组', '第三组', '第四组', '第五组', '第六组', '第七组', '第八组',]
+//     list.forEach((element, index) => {
+//         list[index]['title'] = title[element.group_times]
+//     });
+//     return list
+// }
+// // 查询当前计划组次
+// app.get('/getPlanGroup', (req, res) => {
+//     let sql = `
+//     SELECT distinct my_plan.group_times
+//     FROM my_plan
+//     WHERE my_plan.training_date = '${req.query.training_date}'
+//     `
+//     db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             res.send({ data: setGroup(result) })
+//         }
+//     })
+// })
 
-// 每日动作查询 group_times 组次 ， training_date = 训练日期，星期几
-app.get('/getActionQuery', (req, res) => {
-    let sql = `
-    SELECT a.id,b.action_name,a.weight_claim,a.weight_unit,a.frequency_claim,a.frequency_unit,a.training_order
-    FROM my_plan as a,training_action as b
-    WHERE a.action_id = b.id 
-    AND a.group_times = '${req.query.group_times}'
-    AND a.training_date = '${req.query.training_date}'
-    ORDER BY a.training_order ASC
-    `
-    db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
-        if (err) {
-            console.log(err)
-        } else {
-            res.send({ data: result })
-        }
-    })
-})
+// // 每日动作查询 group_times 组次 ， training_date = 训练日期，星期几
+// app.get('/getActionQuery', (req, res) => {
+//     let sql = `
+//     SELECT a.id,b.action_name,a.weight_claim,a.weight_unit,a.frequency_claim,a.frequency_unit,a.training_order
+//     FROM my_plan as a,training_action as b
+//     WHERE a.action_id = b.id 
+//     AND a.group_times = '${req.query.group_times}'
+//     AND a.training_date = '${req.query.training_date}'
+//     ORDER BY a.training_order ASC
+//     `
+//     db.query(sql, (err, result) => {  // 两个参数，第一个参数固定接收错误
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             res.send({ data: result })
+//         }
+//     })
+// })
 
 
 
 // 开启一个服务器
 app.listen(3000, () => {
-    console.log("127.0.0.1:3000")
+    console.log("127.0.0.4:3000")
 })
 
 
